faa790436dde9e812d1762868c2cefbff1417c62
workingdog <ringowathelet@gmail.com>
2025-11-12 10:04:18 +0900

    Initial Commit

---
+++ ColorSliderDemo/ColorSliderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderDemoApp.swift
+//  ColorSliderDemo
+//
+//  Created by Ringo Wathelet on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorSliderDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorSliderDemo
+//
+//  Created by Ringo Wathelet on 2025/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
