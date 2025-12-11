da171c27dad06eb7474fe919e7777db033e0c242
workingdog <ringowathelet@gmail.com>
2025-12-11 13:40:50 +0900

    minor update

--- ColorSliderDemo/ContentView.swift
+++ ColorSliderDemo/ContentView.swift
@@ -15,9 +15,9 @@ struct ContentView: View {
     var body: some View {
         @Bindable var colorModel = colorModel
         ZStack {
-          //  colorModel.gradient.ignoresSafeArea()
+            // the view background
             colorModel.color.opacity(colorModel.opacity).ignoresSafeArea()
-            
+ 
             VStack(spacing: 20) {
                 Text("\(colorModel.value)")
                 Text(String(colorModel.color.description))
@@ -31,16 +31,26 @@ struct ContentView: View {
                     Spacer()
                 }
                 
-                ColorSlider()
-                    .accentColor(.clear)
-                    .frame(width: 333, height: 40)
-                    .background(colorModel.colorGradient)
+                // the color Slider
+                Slider(value: $colorModel.value, in: colorModel.colorRange, step: 1)
+                    .frame(width: 333, height: 33) // before the background
+                    .background(colorModel.colorGradient) // <--- important
                     .clipShape(RoundedRectangle(cornerRadius: 20))
                     .overlay(RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 1).foregroundColor(.black))
+                    .onChange(of: colorModel.value) {
+                        colorModel.updatePalette()
+                    }
                 
+                // opacity Slider
                 Slider(value: $colorModel.opacity, in: 0...1)
                     .tint(colorModel.color)
                     .frame(width: 333, height: 40)
+                
+                // save the colorModel settings to UserDefaults
+                Button("Save settings") {
+                    colorModel.storeSettings()
+                }.buttonStyle(.borderedProminent)
+ 
             }
             .padding(.top, 30)
             .padding(.horizontal)
@@ -53,6 +63,7 @@ struct ContentView: View {
             // example of using the bandSize to modify the thickness of the color bands
          //   colorModel.nColors = 10
          //   colorModel.bandSize = 10
+            colorModel.retrieveSettings()
         }
     }
 }
